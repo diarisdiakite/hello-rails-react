e2a62404b762512a35ecce50c33e864b85ea64d6
diarisdiakite <diarisdiakite@gmail.com>
2023-12-09 15:08:10 +0100

    Add gretting messages to db/seeds.rb.Seed the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+  'Hello, World!',
+  'Greetings from Rails!',
+  'Welcome to the App!',
+  'Hi there!',
+  'Good to see you!'
+]
+
+greetings.each do |greeting|
+  Message.create(text: greeting)
+end
