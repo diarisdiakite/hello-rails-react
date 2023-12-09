bd0de714992ed8871a8a4148845d49951fee9a8e
diarisdiakite <diarisdiakite@gmail.com>
2023-12-09 15:51:42 +0100

    Add random greetings.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'greetings/random'
   # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'root#index'
+  # root 'root#index'
+
+  get '/random_greeting', to: 'greetings#random', as: 'random_greeting'
+
+  root 'greetings#random'
 end
